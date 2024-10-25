9f1e2f98391bced62d744e65493fe65032f2ae0a
insightbuilder <kamaljp@gmail.com>
2024-10-25 07:44:07 +0530

    updated the data

--- fw_ex/lua_divein/only_chat.lua
+++ fw_ex/lua_divein/only_chat.lua
@@ -1,31 +1,44 @@
 local dotenv = require("lua-dotenv")
 local openai = require("openai")
-l
+
 dotenv.load_dotenv(".env")
 local api_key = dotenv.get("OPENAI_API_KEY")
 
 local client = openai.new(api_key)
+local vimerror = vim.log.levels.ERROR
+local viminfo = vim.log.levels.INFO
 
 local function chat_n_save()
+  -- get cursor location line
   local cursor = vim.api.nvim_win_get_cursor(0)[1]
-  local line = vim.api.nvim_buf_get_lines(0, cursor, -1, false)
-  -- quick usage
-  vim.notify("Request is: " .. line, vim.log.levels.INFO)
+  -- get the entire line where the cursor is
+  vim.notify(tostring(cursor), viminfo)
+  local line = vim.api.nvim_buf_get_lines(0, cursor - 1, -1, false)[1]
+  -- notify the extracted line
+  vim.notify("Request is: " .. line, viminfo)
+  -- initiate a new_chat_session with llm
   local chat = client:new_chat_session({
     message = {
       { role = "system", content = "You are a lua programmer" },
     },
     model = "gpt-4o-mini",
     temperature = 0,
+    max_tokens = 50,
   })
+  -- send the line to llm
   local resp = chat:send(line)
-  print("Recieved the response: ")
-  print(resp)
+
+  -- if line and resp are true
   if line and resp then
-    vim.api.nvim_buf_set_lines(0, cursor[0] + 1, -1, false, resp)
+    local compl = {}
+    table.insert(compl, resp)
+    -- update the response on the next line of the current cursor location
+    vim.notify(resp, viminfo)
+    vim.api.nvim_buf_set_lines(0, cursor + 1, -1, false, compl)
   else
-    vim.notify("Error: Please provide a valid name and age in the buffer.", vim.log.levels.ERROR)
+    -- if false then notify
+    vim.notify("line or resp is missing.", vimerror)
   end
 end
-
+-- command to invoke the above service or function
 vim.api.nvim_create_user_command("LLMComplete", function() chat_n_save() end, {})
