35c0f68ead6ab8dc1b7daba77338c98d8ac10892
insightbuilder <kamaljp@gmail.com>
2024-11-23 07:31:45 +0530

    started sqlite

--- fw_ex/exploring_microservices/microsrv_apps/models.py
+++ fw_ex/exploring_microservices/microsrv_apps/models.py
@@ -1,24 +1,34 @@
-from redis_om import get_redis_connection, Migrator, HashModel
+from typing import Annotated
 
-redis = get_redis_connection(
-    host="34.229.80.21",
-    port=6379,
-    decode_response=True,
-)
+from fastapi import Depends
 
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-class Product(HashModel):
-    name: str
-    price: float
-    quantity: int
-
-    class Meta:
-        database: redis
+# pyright: reportGeneralTypeIssues=false
 
 
-if __name__ == "__main__":
-    product = Product(name="Laptop", price=999.99, quantity=10)
-    product.save()
+class Product(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    qty: int | None = Field(default=None, index=True)
+    secret_name: str
 
-    retrieved = Product.get(product.pk)
-    print(retrieved)
+
+sqlite_file = "product.db"
+sqlite_url = f"sqlite:///{sqlite_file}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+create_db_and_tables()
+# SessionDep = Annotated[Session, Depends(get_session)]
