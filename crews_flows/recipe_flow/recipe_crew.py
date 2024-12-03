a6a035eda39b7c948f335d5b5d32fbf52935fb1d
insightbuilder <kamaljp@gmail.com>
2024-12-03 10:30:29 +0530

    completed the recipe flow

--- crews_flows/recipe_flow/recipe_crew.py
+++ crews_flows/recipe_flow/recipe_crew.py
@@ -6,6 +6,7 @@ from crewai_tools import FileWriterTool
 from dotenv import load_dotenv
 import os
 from pydantic import BaseModel
+from functools import partial
 
 # flake8: noqa E501
 
@@ -15,62 +16,89 @@ llm = LLM(model="gpt-4o-mini", api_key=os.environ["OPENAI_API_KEY"])
 
 # declaring the tool instance for file_write_tool
 
-file_write_tool = FileWriterTool()
+file_write_tool = FileWriterTool(directory=".")
 
 # creating response format
 
 
 class ExtractFormat(BaseModel):
-    dish_name: str
-    number_served: int
+    dish_name: str = ""
+    number_served: int = 5
+    file_name: str = ""
 
 
 # creating extraction agent
 
 extraction_agent = Agent(
-    role="You Extract the names, quantity from given input.",
-    goal="Extract the details like name and number of people to be served",
+    role="You Extract the names of dishes, files, and the quantity or numbers from given input.",
+    goal="Extract the details like dish_name, file_name and number of people to be served",
     backstory="You are expert in structuring any prose, and extracting information. You are brief and to the point.",
     llm=llm,
+    verbose=True,
 )
 
 extraction_task = Task(
     description="Extract the dish_name and number_of_people to be served from {input}",
-    expected_output="Json formated dish_name and number_served",
+    expected_output="Json formated dish_name, number_served and file_name",
     agent=extraction_agent,
     output_pydantic=ExtractFormat,
 )
 
-Extraction_Crew = Crew(name="extraction_crew", tasks=[extraction_task], verbose=True)
+
+def on_extraction_complete(extract):
+    print(f"Extraction Output: {extract}")
+
+
+Extraction_Crew = Crew(
+    name="extraction_crew",
+    tasks=[extraction_task],
+    verbose=True,
+    # step_callback=partial(on_extraction_complete, extract="done"),
+)
+
+
+class ChefFormat(BaseModel):
+    recipe_data: str
+
 
 chef_agent = Agent(
     role="Writing recipes for the dishes asked by the users",
-    goal="Provide the detailed recipe for {dish_name} to serve {number_served} with quantity of ingredients to be used",
+    goal="Provide the short to the point recipe for {dish_name} to serve {number_served} with quantity of ingredients to be used",
     backstory="You are michelin star rated Master chef with culinary skills ranging from western to eastern region.You are brief and to the point.",
     llm=llm,
+    verbose=True,
 )
 
 chef_task = Task(
     description="Write the step by step guide for making {dish_name} to serve {number_served}",
     expected_output="Recipe for the {dish_name} along with quantity of ingredients, in markdown format",
     agent=chef_agent,
+    output_pydantic=ChefFormat,
 )
+
+
+def on_recipe_complete(recipe):
+    print(f"Chef creating Recipe:{recipe}")
+
 
 Chef_Crew = Crew(
     name="chef_crew",
     tasks=[chef_task],
     agents=[chef_agent],
     verbose=True,
+    # step_callback=partial(on_recipe_complete, recipe="done"),
 )
 
 writer_agent = Agent(
-    role="You write the data to the file located at {file_path}",
+    role="""You write the data to the file located at {file_path}
+    if file_path is not provided the write to recipe.md file in current directory""",
     goal="Write the text data to the file and update the user on completion",
     backstory="""You are excellent file-system manager, and expert at file writitng.,
     You are brief and to the point.
-    You can make the file by using the {name} and fill it {data}""",
+    You can make the file by using the {file_path} and fill it {recipe_data}""",
     llm=llm,
     tools=[file_write_tool],
+    verbose=True,
 )
 
 writer_task = Task(
@@ -78,6 +106,11 @@ writer_task = Task(
     expected_output="The status of file writing",
     agent=writer_agent,
 )
+
+
+def on_write_complete():
+    print("Writing file: Completed")
+
 
 Writer_Crew = Crew(
     name="writer_crew", tasks=[writer_task], agents=[writer_agent], verbose=True
--- crews_flows/recipe_flow/recipe_flow.py
+++ crews_flows/recipe_flow/recipe_flow.py
@@ -13,29 +13,39 @@ from recipe_crew import Chef_Crew, Extraction_Crew, Writer_Crew  # noqa: pyright
 class RecipeState(BaseModel):
     input: str = ""
     recipe_data: str = ""
+    file_name: str = ""
 
 
 class RecipeFlow(Flow[RecipeState]):
     @start()
     def extraction(self):
         extraction_crew_output = Extraction_Crew.kickoff({"input": self.state.input})
-        raw_output = extraction_crew_output.raw
-        print("raw_output", raw_output)
-        print("pydantic", extraction_crew_output.pydantic)
+        # print("pydantic", extraction_crew_output.pydantic)
+        # store the file name
+        self.state.file_name = extraction_crew_output.pydantic.file_name
+        return extraction_crew_output.pydantic
 
     @listen(extraction)
     def create_recipe(self, extraction_output):
-        print(f"Extraction in side create")
-        # chef_crew_output = chef_crew.kickoff(extraction_output)
-        # return chef_crew_output
+        # print(f"Extraction in side create {extraction_output}")
+        dict_input = extraction_output.dict()
+        chef_crew_output = Chef_Crew.kickoff(dict_input)
+        return chef_crew_output.pydantic
 
     @listen(create_recipe)
     def write_recipe(self, create_recipe_out):
-        print(f"Write recipe:")
+        # print(f"Write recipe: {create_recipe_out}")
+        to_write = create_recipe_out.dict()
+        to_write["file_path"] = self.state.file_name
+        print(f"Assembled to_write: {to_write}")
+        writer_crew_output = Writer_Crew.kickoff(to_write)
+        return writer_crew_output
 
 
 makerecipe = RecipeFlow()
 flow_output = makerecipe.kickoff(
-    {"input": "Recipe for rice dumplings to serve 15 people"}
+    {
+        "input": "Provide the Recipe for rice dumplings to serve 15 people, and write to chef_recipe.md"
+    }
 )
 print(flow_output)
