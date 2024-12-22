c4bed8ec2c3799f2875ac179ad78b3d6c9665a42
insightbuilder <kamaljp@gmail.com>
2024-12-22 22:10:40 +0530

    completed composio workspace

--- crews_flows/all_tools_test/composio_filetool_direct.py
+++ crews_flows/all_tools_test/composio_filetool_direct.py
@@ -1,7 +1,9 @@
 from composio_crewai import ComposioToolSet, Action
 import os
+from openai import OpenAI
 
 toolset = ComposioToolSet(api_key=os.getenv("COMPOSIO_API_KEY"))
+openai_client = OpenAI()
 
 # connected_account_id = toolset.get_connected_account_id()
 
@@ -10,12 +12,24 @@ tools = toolset.get_tools(actions=["FILETOOL_CREATE_FILE"])
 # print(tools[0])
 # print(len(tools))
 
+
 # toolset.execute_action(
-#     action=Action.FILETOOL_OPEN_FILE,
-#     params={"file_path": "composio_start.py"},
+#     action=Action.FILETOOL_CREATE_FILE,
+#     params={"path": "composio_test.py"},
 # )
 
-toolset.execute_action(
-    action=Action.FILETOOL_CREATE_FILE,
-    params={"path": "composio_test.py"},
+task = "list the files in the directory with tools"
+
+response = openai_client.chat.completions.create(
+    model="gpt-4o-mini",
+    tools=tools,
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": task},
+    ],
 )
+print("Response is: {response}")
+
+result = toolset.handle_tool_calls(response)
+
+print(result)
--- crews_flows/all_tools_test/composio_workspace_host.py
+++ crews_flows/all_tools_test/composio_workspace_host.py
@@ -1,12 +1,73 @@
-from composio_crewai import ComposioToolSet, WorkspaceType, Action, App
+# from composio_crewai import ComposioToolSet, WorkspaceType, Action, App
+# the LLM has to be used for operating on the workspace
 
-toolset = ComposioToolSet(workspace_config=WorkspaceType.Host())
+from composio_openai import ComposioToolSet, WorkspaceType, App, Action
+import os
+from openai import OpenAI
+from rich import print
 
-print(toolset.get_tools(apps=[App.FILETOOL]))
-
-response = toolset.execute_action(
-    action=Action.FILETOOL_LIST_FILES,
-    # params={"path": "/home/uberdev"},
+openai_client = OpenAI()
+toolset = ComposioToolSet(
+    workspace_config=WorkspaceType.Host(),
+    api_key=os.getenv("COMPOSIO_API_KEY"),
 )
 
-print(response)
+tools = toolset.get_tools(apps=[App.FILETOOL])
+
+
+def print_tools(tools):
+    for tool in tools:
+        # print(type(tool))
+        print(tool["function"]["name"])
+        # print(tool["function"]["parameters"].keys())
+        if "required" in tool["function"]["parameters"]:
+            print("required: ", tool["function"]["parameters"]["required"])
+            print("title: ", tool["function"]["parameters"]["title"])
+            print("properties: ", tool["function"]["parameters"]["properties"])
+        else:
+            print("No required fields")
+
+
+# response = openai_client.chat.completions.create(
+#     model="gpt-4o-mini",
+#     tools=tools,
+#     messages=[
+#         {"role": "system", "content": "You are a helpful assistant."},
+#         {"role": "user", "content": "Change directory to /home/uberdev"},
+#     ],
+# )
+
+# # print(response)
+
+# result = toolset.handle_tool_calls(response)
+
+# print("result to change working directory: ", result)
+
+# response = openai_client.chat.completions.create(
+#     model="gpt-4o-mini",
+#     tools=tools,
+#     messages=[
+#         {"role": "system", "content": "You are a helpful assistant."},
+#         {"role": "user", "content": "list the files in the directory"},
+#     ],
+# )
+
+# # print(response)
+
+# result = toolset.handle_tool_calls(response)
+
+# print("result for listing dir: ", result)
+
+
+toolset.execute_action(
+    action=Action.FILETOOL_CHANGE_WORKING_DIRECTORY,
+    params={"path": "/home/uberdev"},
+    # Natural language prompt
+    # text="change directory to /home/uberdev",
+)
+toolset.execute_action(
+    action=Action.FILETOOL_LIST_FILES,
+    params={},
+    # Natural language prompt
+    text="list the files in the directory",
+)
